ce5decd30e673309f7e34a1d39ed10702f63bbe1
Langxxx <316230604@qq.com>
2018-02-07 11:25:15 +0800

    Initial Commit

---
+++ BCMarkdown/BCMarkdown.h
@@ -0,0 +1,19 @@
+//
+//  BCMarkdown.h
+//  BCMarkdown
+//
+//  Created by lang on 07/02/2018.
+//  Copyright © 2018 Beary Innovative. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BCMarkdown.
+FOUNDATION_EXPORT double BCMarkdownVersionNumber;
+
+//! Project version string for BCMarkdown.
+FOUNDATION_EXPORT const unsigned char BCMarkdownVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BCMarkdown/PublicHeader.h>
+
+
